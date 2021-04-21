061df1166f34334de2f5f467e487866f83152ecb
jevesh14 <76529483+jevesh14@users.noreply.github.com>
2021-04-21 16:28:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(640,480);
+     canvas.position(200,200);
+     video = createCapture(VIDEO);
+     video.hide();
+}
+
+function draw(){
+    image(video ,230,150,220,200);
+
+    fill(255,0,0);
+     stroke(255,0,0);
+     
+
+     fill(0,128,0);
+     stroke(0,128,0);
+     rect(90,40,460,20);
+
+     rect(90,420,460,20);
+}
+
+function take_snapshot(){
+save("photo.png")
+}
